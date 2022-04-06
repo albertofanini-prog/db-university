39fa1567bdaae9d7d21aa07c11e6b370df527634
albertofanini-prog <albertofanini@outlook.com>
2022-04-06 15:28:46 +0200

    2nd query - group by

--- query2/query.sql
+++ query2/query.sql
@@ -2,4 +2,9 @@
 --- Contare quanti iscritti ci sono ogni anno
 SELECT YEAR(`enrolment_date`) AS `enr`, COUNT(`id`)
 FROM (`students`)
-GROUP BY(`enr`)
+GROUP BY(`enr`);
+
+--- Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT (`office_address`), COUNT(*) AS `teachers_here`
+FROM `teachers`
+GROUP BY `office_address`
