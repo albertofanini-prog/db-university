d1a5e36afda59aef5d753341040fa139e66bfa54
albertofanini-prog <albertofanini@outlook.com>
2022-04-06 15:37:02 +0200

    3rd query - group by

--- query2/query.sql
+++ query2/query.sql
@@ -2,7 +2,7 @@
 --- Contare quanti iscritti ci sono ogni anno
 SELECT YEAR(`enrolment_date`) AS `enr`, COUNT(`id`)
 FROM (`students`)
-GROUP BY(`enr`);
+GROUP BY `enr`;
 
 --- Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 SELECT (`office_address`), COUNT(*) AS `teachers_here`
@@ -12,4 +12,4 @@ GROUP BY `office_address`;
 --- Calcolare la media dei voti di ogni appello d'esame
 SELECT (`exam_id`), AVG(`vote`) AS `media`
 FROM `exam_student`
-GROUP BY (`exam_id`)
+GROUP BY `exam_id`;
