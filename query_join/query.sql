fc2fb44fe0213fc9316c763b0c37148c6280e84d
albertofanini-prog <albertofanini@outlook.com>
2022-04-07 11:08:56 +0200

    3rd query - Join

--- query_join/query.sql
+++ query_join/query.sql
@@ -9,5 +9,15 @@ WHERE `degrees`.`name` = 'Corso di Laurea in Economia'
 --- Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 SELECT `departments`.`name`, `degrees`.*
 FROM `departments`
-INNER JOIN `degrees` ON `degrees`.`department_id` = `departments`.`id`
+INNER JOIN `degrees`
+ON `degrees`.`department_id` = `departments`.`id`
 WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
+
+--- Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT `t`.`name`, `t`.`surname`, `c`.`name` AS course_name
+FROM `teachers` AS `t`
+JOIN `course_teacher` AS `ct`
+ON `ct`.`teacher_id` = `t`.`id`
+JOIN `courses` AS `c`
+ON `c`.`id` = `ct`.`course_id`
+WHERE `t`.`id` = 44;
